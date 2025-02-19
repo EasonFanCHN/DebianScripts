d19adacb86da58ba2cfbd59f8a101374cfaa00a1
Eason Fan <jerryfan2013@163.com>
2025-02-19 14:34:33 +0800

    update scripts

--- ohmyzsh_setup.sh
+++ ohmyzsh_setup.sh
@@ -8,8 +8,22 @@ if [ "$(id -u)" -eq 0 ]; then
     exit 1
 fi
 
-# Install dependencies
-sudo apt update && sudo apt install -y zsh curl git
+# Detect OS and install dependencies accordingly
+OS="$(uname)"
+if [ "$OS" == "Linux" ]; then
+    echo "Detected Linux system. Installing dependencies..."
+    sudo apt update -qq && sudo apt install -y zsh curl git
+elif [ "$OS" == "Darwin" ]; then
+    echo "Detected macOS. Installing dependencies..."
+    if ! command -v brew &>/dev/null; then
+        echo "Homebrew not found. Please install Homebrew first: https://brew.sh/"
+        exit 1
+    fi
+    brew install zsh curl git
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
 
 # Install Oh My Zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
@@ -20,25 +34,34 @@ else
 fi
 
 # Set Oh My Zsh theme to geoffgarside
-sed -i 's/^ZSH_THEME=.*/ZSH_THEME="geoffgarside"/' "$HOME/.zshrc"
+sed -i '' 's/^ZSH_THEME=.*/ZSH_THEME="geoffgarside"/' "$HOME/.zshrc"
 
 # Install zsh-autosuggestions plugin
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
     echo "Installing zsh-autosuggestions plugin..."
-    git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+    git clone --quiet https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 fi
 
 # Install zsh-syntax-highlighting plugin
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
     echo "Installing zsh-syntax-highlighting plugin..."
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+    git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 fi
+
+# Install zsh-git-prompt plugin
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-git-prompt" ]; then
+    echo "Installing zsh-git-prompt plugin..."
+    git clone --quiet https://github.com/olivierverdier/zsh-git-prompt "$ZSH_CUSTOM/plugins/zsh-git-prompt"
+fi
+
+# Enable plugins in .zshrc
+sed -i '' 's/^plugins=(.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-git-prompt)/' "$HOME/.zshrc"
 
 # Change the default shell to Zsh
 if [ "$SHELL" != "$(which zsh)" ]; then
     echo "Changing default shell to Zsh..."
-    chsh -s "$(which zsh)"
+    chsh -s "$(which zsh)" "$USER"
 fi
 
 echo "Oh My Zsh installation completed! Restart your terminal or log out and back in for changes to take effect."
